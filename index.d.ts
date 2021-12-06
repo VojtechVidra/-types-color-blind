8a6e53bb2635abcab3778dcf3fa3f7f9ef6c0f2b
Vojtěch Vidra <vojtechvidra@gmail.com>
2021-12-06 09:28:01 +0100

    Initial commit

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+declare module "color-blind" {
+  export function protanomaly(hex: string): string;
+  export function protanopia(hex: string): string;
+  export function deuteranomaly(hex: string): string;
+  export function deuteranopia(hex: string): string;
+  export function tritanomaly(hex: string): string;
+  export function tritanopia(hex: string): string;
+  export function achromatomaly(hex: string): string;
+  export function achromatopsia(hex: string): string;
+}
